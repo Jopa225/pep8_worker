81d99af41300509e407e11fa1dc84f4deeaba9e4
Jopa225 <jopapocrnja22@gmail.com>
2024-03-03 13:05:12 +0100

    Change to pep8_worker file

--- pep8_reworker.py
+++ pep8_reworker.py
@@ -11,18 +11,45 @@
 
 import argparse
 import os
+import re
+import shutil
 
-path = "pep8_folders"
 
 parser = argparse.ArgumentParser(description="Trace event analyzer.")
-parser.add_argument("--folder_path", type=str, help="Set the folder path to parse")
+parser.add_argument("--folder_path", "-f", type=str,
+                    help="Set the folder path to parse")
+parser.add_argument("--apply_changes", "-a", action="store_true",
+                    help="Apply changes that pep8parser made in _temp \
+                        file to actual file")
 
 args = parser.parse_args()
 
-# print(args.folder_path)
+
+def copy_py_files(py_files):
+    copied_files = list()
+    for file in py_files:
+        file_copy = file[:-3] + "_temp" + file[-3:]
+        if "_temp" not in file:
+            print(f"Copying file {file}")
+            shutil.copyfile(file, file_copy)
+            copied_files.append(file_copy)
+    return copied_files
+
+
+def apply_file_changes(py_files):
+    for file in py_files:
+        if "_temp" in file:
+            continue
+        file_copy = file[:-3] + "_temp" + file[-3:]
+        print(f"Apply changes to original file {
+              file}, then remove {file_copy}")
+        shutil.copy(file_copy, file)
+        if os.path.exists(file_copy):
+            os.remove(file_copy)
 
 
 def get_py_files_paths_list(folder_path):
+    print(f"Finding all .py files in {folder_path}")
     py_files = []
     for roots, dirs, files in os.walk(folder_path):
         for file in files:
@@ -32,6 +59,7 @@ def get_py_files_paths_list(folder_path):
 
 
 def read_files_data(files_path):
+    print(f"Reading data of all files and storing it in \"data\" variable")
     data = []
     for files in files_path:
         with open(files, "r", encoding="utf8") as file:
@@ -40,52 +68,47 @@ def read_files_data(files_path):
 
 
 def write_data_to_files(files_path, data):
-    i = 0
-    for files in files_path:
+    print("Writing \"data\" to _temp files")
+    for i, files in enumerate(files_path):
         while i < len(files_path):
             with open(files, "w", encoding="utf8") as file:
                 file.writelines(data[i])
             break
-        i +=1
 
 
 def pep8_append_EOF(data):
+    print("Append EOF")
     for lst in data:
-        if lst[-1][-1] != "\n":
+        if "\n" not in lst[-1][-1]:
             lst.append("\n")
 
 
 def pep8_imports(data):
     for lst in data:
-        j = 0
         idx_import = []
-        for row in lst:
+        for j, row in enumerate(lst):
             if "from" in row and "," in row and "#" not in row:
-                string_to_add = " "
-                pep8_idx = row.index("from")
                 from_word = row[row.index(" "):row.index("import")]
-                lst[j] = row.replace(", ", string_to_add * pep8_idx + "\nfrom" + from_word + "import")
+                lst[j] = row.replace(", ", "\nfrom" + from_word + "import")
             elif "import" in row and "," in row:
-                string_to_add = " "
-                pep8_idx = row.index("import")
-                lst[j] = row.replace(", ", string_to_add * pep8_idx + "\nimport")
+                lst[j] = row.replace(", ",  "\nimport ")
 
-            if row.startswith("import") or row.startswith("from"):
+            if "import" in row:
                 idx_import.append(lst[j])
 
-            j += 1
+        # Delete all imports
         for x in idx_import:
-            del(lst[lst.index(x)])
+            del (lst[lst.index(x)])
+        # Set all imports at start of file
         for x in reversed(idx_import):
-            lst.insert(0, x)
+            lst.insert(0, x.lstrip())
 
 
-###################                   replace for row in lst: with     for j, row in enumerate(lst):      remove j += 1 i j = 0
 def pep8_line_ending79(data):
     for lst in data:
-        j = 0
-        for row in lst:
-            if len(row) > 79: 
+        for j, row in enumerate(lst):
+            if len(row) > 79:
+                print(f"Detected line over 79 characters at index: {j + 1}")
                 idx = row.rindex(" ", 1, 79)
                 new_line = " \\\n"
                 if "=" in row and "def" not in row:
@@ -99,105 +122,110 @@ def pep8_line_ending79(data):
                     pep8_idx = 1
                     string_to_add = "#"
                     new_line = "\n"
-                
-                row = row[:idx] + new_line + string_to_add * pep8_idx + row[idx:]
+
+                row = row[:idx] + new_line + \
+                    string_to_add * pep8_idx + row[idx:]
                 row = row.split("\n")[:-1]
-                
+
                 while len(row[-1]) > 79:
                     idx = row[-1].rindex(" ", 1, 79)
-                    row[-1] = row[-1][:idx] + new_line + string_to_add * pep8_idx + row[-1][idx:]
+                    row[-1] = row[-1][:idx] + new_line + \
+                        string_to_add * pep8_idx + row[-1][idx:]
                     row.append(" ")
-                    row[-2], row[-1] = row[-2].split("\n")[-2], row[-2].split("\n")[-1]
+                    row[-2], row[-1] = row[-2].split(
+                        "\n")[-2], row[-2].split("\n")[-1]
                     if len(row[-1]) < 79:
                         break
-                    
-                lst[j] = "\n".join(row)
+
+                lst[j] = "\n".join(row) + "\n"
                 string_to_add = " "
-            j += 1
-
-
-def pep8_trailing_whitespaces(data):
-    for lst in data:
-        j = 0
-        for row in lst:
-            for i, element in enumerate(row):
-                pass
-                # if element == " " and row[i+1] == " ":
-                #     row[i] = row[i].translate({32: None})#######################rip
-            # if row.startswith(" ") not in row:
-            # # for element in row:
-            #     pass
 
 
 def pep8_whitespaces(data):
     for lst in data:
-        j = 0
-        for row in lst:
-            if "=" in row and "def" not in row:
-                lst[j] = row.replace("=", " = ")
+        for j, row in enumerate(lst):
+            if "=" in row and "def" not in row and re.findall(r"=([+\-*\/%^&|<>])", row) and "==" not in row:
+                row = row.replace("=", " = ")
             if "," in row and ",)" not in row:
-                lst[j] = row.replace(",", ", ")
-            if "\t" in row:
-                lst[j] = row.replace("\t", "    ")
-            
-            j += 1
+                row = row.replace(",", ", ")
+            if "\t" in row and "\"" not in row:
+                row = row.replace("\t", "    ")
+
+            # Remove all extra spaces not including indent
+            if re.search('[a-zA-Z]', row):
+                idx = re.search('[a-zA-Z]', row)
+                if idx is not None:
+                    idx = idx.start()
+                saved_spaces = ""
+                if not row.isspace() and not "\"\"\"" in row:
+                    saved_spaces = row[:idx]
+                row = re.sub(" +", " ", row[idx:])
+
+                if " =" in row and "def" in row:
+                    row = row.replace(" =", "=")
+                if "= " in row and "def" in row:
+                    row = row.replace("= ", "=")
+                if " = " in row and "def" in row:
+                    row = row.replace(" = ", "=")
+
+                lst[j] = saved_spaces + row
 
 
 def pep8_before_func_newline(data):
     for lst in data:
         del_data = []
-        j = 0
-        for row in lst:
+        for j, row in enumerate(lst):
+            if row.isspace():
+                del_data.append(j)
 
-            if row.startswith("\n") or row.startswith("    \n"):
-                del_data.append(j)  
-            if "def " in row and "self" not in row:
-                lst[j] = "\n" + row
-            idx_1 = j - 1 
+            idx_1 = j - 1
             idx_2 = j - 2
-            
-            if "def " in row and (lst[idx_2] == "\n" and lst[idx_1] != "\n"):
-                lst[j] = "\n\n" + row
-            j += 1
+            # Add the needed numbers of newlines before function definition
+            if "def " in row and "self" in row and lst[idx_1].isspace():
+                lst[j] = row
+            elif "def " in row and "self" in row:
+                lst[j] = "\n" + row
+            else:
+                if "def " in row and not lst[idx_2].isspace() and not lst[idx_1].isspace():
+                    lst[j] = "\n\n" + row
+                if "def " in row and (lst[idx_2].isspace() and not lst[idx_1].isspace()):
+                    lst[j] = "\n\n" + row
+                if "def " in row and (lst[idx_1].isspace() and not lst[idx_2].isspace()):
+                    lst[j] = "\n" + row
+                if "def " in row and (lst[idx_1].isspace() and lst[idx_2].isspace()):
+                    lst[j] = "\n" + row
+
+        # Remove extra newlines between all lines in file
         for x in reversed(del_data):
             if (x-1) in del_data:
-                del(lst[x])
-        # if "\n" in lst[0]:
-        #     del(lst[0]) 
-        if lst[-1].startswith("\n"):
-            del(lst[-1]) 
-        
+                print(f"Remove extra newline at index {x}")
+                del (lst[x])
+        if lst[-1].isspace():
+            del (lst[-1])
+
 
 def main(path):
+
     py_files = get_py_files_paths_list(path)
-    data = read_files_data(py_files)
-    
-    pep8_whitespaces(data)
-    
-    pep8_before_func_newline(data)
 
-    pep8_imports(data)
+    if args.apply_changes:
+        apply_file_changes(py_files)
+    else:
+        copied_py_files_paths = copy_py_files(py_files)
 
-    
-    
-    pep8_line_ending79(data)
+        data = read_files_data(copied_py_files_paths)
 
-    pep8_append_EOF(data)
+        pep8_line_ending79(data)
 
-    pep8_trailing_whitespaces(data)
-    # print(len(data))
-    print(data[0])
-    print(data[-1])
-    print(len(py_files))
-    print(len(data))
+        pep8_whitespaces(data)
+        pep8_imports(data)
+
+        pep8_before_func_newline(data)
+        pep8_append_EOF(data)
+
+        write_data_to_files(copied_py_files_paths, data)
 
 
-
-
-    # write_data_to_files(py_files, data)
-
-
-main(path)
-# main(args.folder_path)
-
-
+# path = "pep8_folders\day6"
+# main(path)
+main(args.folder_path)
