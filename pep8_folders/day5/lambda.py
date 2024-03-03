5e9fc4a6f327038782a3ac4d55184354448569dd
Josip Pocrnja <82735869+Jopa225@users.noreply.github.com>
2024-03-03 13:06:54 +0100

    Delete pep8_folders directory

--- pep8_folders/day5/lambda.py
+++
@@ -1,6 +0,0 @@
-def foo(n):
-    return lambda x: x**n
-
-exp = foo(3)
-print(exp(2))
-
--- pep8_folders/day9/spaces.py
+++
@@ -1,7 +0,0 @@
-
-import os
-
-def fja(AAAAAAAAAAAAAAAAAA, AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA, AAAAAAAAAAAAAAAA, AAAAAAAAAAAAAAAAAAAAA, aaaaaaaaaaaaaaaaaaaaaa, aaaaaaa, aa, a, bbbbbbbbbbbb, bbbbbbbbbbbbbbbbbbbb, bbbbbbb, bbbbbbbbbbb, bbbbbbbbbbb):
-    pass
-
-# hey
